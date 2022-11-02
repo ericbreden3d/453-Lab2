6cc8b749dd5180d878bab25a6b9a82c9d15188c0
Breden <ebrede2@uic.edu>
2022-11-02 16:30:41 -0500

    first

--- lab2.cpp
+++ lab2.cpp
@@ -1,27 +1,48 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <string>
 #include <mpi.h>
 using namespace std;
 
-void make_matrix(int n) {
-    srand(time(NULL));
-    int** m = new int*[n];
-    for (int i = 0; i < n; i++) {
-        cout << m[i] << endl;
+class Matrix {
+    int** matrix;
+    int size;
+
+ public:
+    Matrix(int n) {
+        size = n;
+        int** m = new int*[n];
+        for (int i = 0; i < n; i++) {
+            m[i] = new int[n];
+            for (int j = 0; j < n; j++) {
+                m[j][i] = 3 - rand() % 4;
+            }
+        }
+    }
+
+    void print_matrix() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << matrix[j][i] << " ";
+            }
+            cout << endl;
+        }
     }
 }
 
 int main(int argc, char** argv) {
     int this_rank;
     int num_procs;
+    int n = stoi(argv[1])
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &this_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
     if (this_rank == 0) {
-        make_matrix(10);
+        make_matrix(n);
+        print_matrix(m);
     }
 }
 
