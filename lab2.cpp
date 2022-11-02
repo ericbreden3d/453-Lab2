63e6ea34befbed1b3d8b8832e4194e34967c65a0
Breden <ebrede2@uic.edu>
2022-11-02 16:44:44 -0500

    first

--- lab2.cpp
+++ lab2.cpp
@@ -22,12 +22,12 @@ class Matrix {
         // }
     }
 
-    ~Matrix() {
-        for (int i = 0; i < size; i++) {
-            delete[] matrix[i];
-        }
-        delete[] matrix;
-    }
+    // ~Matrix() {
+    //     for (int i = 0; i < size; i++) {
+    //         delete[] matrix[i];
+    //     }
+    //     delete[] matrix;
+    // }
 
     void print_matrix() {
         for (int i = 0; i < size; i++) {
