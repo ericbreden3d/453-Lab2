190c741e311f34a80ed11031ec34b17fc90776fe
Breden <ebrede2@uic.edu>
2022-11-02 16:11:38 -0500

    first

--- lab2.cpp
+++ lab2.cpp
@@ -5,12 +5,24 @@ using namespace std;
 
 void make_matrix(int n) {
     srand(time(NULL));
-    int m[n] = {};
+    int** m = new int*[n];
     for (int i = 0; i < n; i++) {
         cout << m[i] << endl;
     }
 }
 
-int main() {
-    make_matrix(10);
+int main(int argc, int** argv) {
+    int this_rank;
+    int num_procs;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &this_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+    if (this_rank == 0) {
+        make_matrix(10);
+    }
 }
+
+
+// free
