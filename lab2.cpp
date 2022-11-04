955ef80b236c37327448b138367938e095958086
Breden <ebrede2@uic.edu>
2022-11-04 13:58:33 -0500

    new

--- lab2.cpp
+++ lab2.cpp
@@ -58,9 +58,6 @@ int main(int argc, char** argv) {
             }
         }
 
-        // for (int i = 0; i < num_procs; i++) {
-        //     parts[i].print();
-        // }
         ind = 1;
         for (int i = 0; i < dims[0]; i++) {
             for (int j = 0; j < dims[1]; j++) {
@@ -82,7 +79,23 @@ int main(int argc, char** argv) {
         B = A;
     }
 
-    cout << A.determinant() << endl;    
+    // Initial Alignment
+    for (int i = 0; i < dims[0]; i++) {
+        for (int j = 0; j < dims[1]; j++) {
+            if (this_coord[0] == i && this_coord[1] == j) {
+                int src_rank;
+                int A_dest;
+                int B_dest;
+                MPI_Cart_shift(cart_comm, 0, i, &src_rank, &A_dest);
+                MPI_Cart_shift(cart_comm, 1, j, &src_rank, &B_dest);
+                MPI_Isend(A.get_1d(), sub_n * sub_n, MPI_INT, targ_A, 0, cart_comm, &req);
+                MPI_Isend(B.get_1d(), sub_n * sub_n, MPI_INT, targ_B, 0, <<cart_comm, &req);
+                int A_coord[2];
+                MPI_Cart_rank(cart_comm, targ_A, 2, A_coord);
+                cout << i << "," << j << " " << "sending to " << A_coord[0] << "," << A_coord[1] << endl;
+            }
+        }
+    }
     
 
 
