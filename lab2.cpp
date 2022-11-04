5ec62f87309599ed8939ea80289ea9f85f653c71
Breden <ebrede2@uic.edu>
2022-11-04 18:45:34 -0500

    new

--- lab2.cpp
+++ lab2.cpp
@@ -155,8 +155,8 @@ int main(int argc, char** argv) {
         parts[0] = sum;
         for (int i = 0; i < num_procs; i++) {
             int buf[sub_n * sub_n];
-            MPI_Recv(buf, sub_n * sub_n, MPI_INT, i, 0, cart_comm, &stat);
             cout << i << " received" << endl;
+            MPI_Recv(buf, sub_n * sub_n, MPI_INT, i, 0, cart_comm, &stat);
             int coord[2];
             MPI_Cart_coords(cart_comm, i, 2, coord);
             parts[coord[0] + coord[1] * dims[0]] = Matrix(buf, sub_n);
