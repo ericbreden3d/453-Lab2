13154febeb70c6a6becace4f369c10be7044fd2f
Breden <ebrede2@uic.edu>
2022-11-04 15:21:24 -0500

    new

--- lab2.cpp
+++ lab2.cpp
@@ -79,7 +79,7 @@ int main(int argc, char** argv) {
         B = A;
     }
 
-    if (this_rank == 2) {
+    if (this_rank == 3) {
         cout << this_coord[0] << "," << this_coord[1] << " has" << endl;
         A.print();
     }
