2f5e529d0bf61300176afc21556970eeb32fe58d
Breden <ebrede2@uic.edu>
2022-11-04 15:05:18 -0500

    new

--- lab2.cpp
+++ lab2.cpp
@@ -85,31 +85,31 @@ int main(int argc, char** argv) {
     return;
 
     // Initial Send Alignment
-    int A_src;
-    int B_src;
-    int A_dest;
-    int B_dest;
-    if (this_coord[0] != 0) {
-        MPI_Cart_shift(cart_comm, 0, this_coord[0], &A_src, &A_dest);
-        MPI_Isend(A.get_1d(), sub_n * sub_n, MPI_INT, A_dest, 0, cart_comm, &req);
-    }
-    if (this_coord[1] != 0) {
-        MPI_Cart_shift(cart_comm, 1, this_coord[1], &B_src, &B_dest);
-        MPI_Isend(B.get_1d(), sub_n * sub_n, MPI_INT, B_dest, 0, cart_comm, &req);
-    }
-    if (this_coord[0] != 0){
-        int buf[sub_n*sub_n];
-        MPI_Recv(buf, sub_n*sub_n, MPI_INT, A_src, 0, cart_comm, &stat);
-        A = Matrix(buf, sub_n);
-    }
-    if (this_coord[1] != 0) {
-        int buf[sub_n*sub_n];
-        MPI_Recv(buf, sub_n*sub_n, MPI_INT, B_src, 0, cart_comm, &stat);
-        B = Matrix(buf, sub_n);
-    }
+    // int A_src;
+    // int B_src;
+    // int A_dest;
+    // int B_dest;
+    // if (this_coord[0] != 0) {
+    //     MPI_Cart_shift(cart_comm, 0, this_coord[0], &A_src, &A_dest);
+    //     MPI_Isend(A.get_1d(), sub_n * sub_n, MPI_INT, A_dest, 0, cart_comm, &req);
+    // }
+    // if (this_coord[1] != 0) {
+    //     MPI_Cart_shift(cart_comm, 1, this_coord[1], &B_src, &B_dest);
+    //     MPI_Isend(B.get_1d(), sub_n * sub_n, MPI_INT, B_dest, 0, cart_comm, &req);
+    // }
+    // if (this_coord[0] != 0){
+    //     int buf[sub_n*sub_n];
+    //     MPI_Recv(buf, sub_n*sub_n, MPI_INT, A_src, 0, cart_comm, &stat);
+    //     A = Matrix(buf, sub_n);
+    // }
+    // if (this_coord[1] != 0) {
+    //     int buf[sub_n*sub_n];
+    //     MPI_Recv(buf, sub_n*sub_n, MPI_INT, B_src, 0, cart_comm, &stat);
+    //     B = Matrix(buf, sub_n);
+    // }
 
-    MPI_Barrier(cart_comm);
-    cout << "Initial alignment complete\n";
+    // MPI_Barrier(cart_comm);
+    // cout << "Initial alignment complete\n";
 }
 
 
