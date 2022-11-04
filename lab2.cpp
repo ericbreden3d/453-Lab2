5325ee0d536e547184ca5f1029def7c7a2c4b5ac
Breden <ebrede2@uic.edu>
2022-11-04 15:05:47 -0500

    new

--- lab2.cpp
+++ lab2.cpp
@@ -28,7 +28,7 @@ int main(int argc, char** argv) {
     MPI_Status stat;
     Matrix A;
     Matrix B;
-
+    return 0;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &this_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
