b1824e46031034588b7c59f4f6336e87271f6cb2
Breden <ebrede2@uic.edu>
2022-11-05 15:25:56 -0500

    new

--- lab2.cpp
+++ lab2.cpp
@@ -125,10 +125,12 @@ int main(int argc, char** argv) {
         MPI_Cart_shift(cart_comm, 0, 1, &B_src, &B_dest);
         MPI_Isend(A.get_1d(), sub_n * sub_n, MPI_INT, A_dest, 0, cart_comm, &req);
         MPI_Isend(B.get_1d(), sub_n * sub_n, MPI_INT, B_dest, 0, cart_comm, &req);
-        MPI_Recv(buf, sub_n*sub_n, MPI_INT, A_src, 0, cart_comm, &stat);
-        A = Matrix(buf, sub_n);
-        MPI_Recv(buf, sub_n*sub_n, MPI_INT, B_src, 0, cart_comm, &stat);
-        B = Matrix(buf, sub_n);
+        int buf1[sub_n*sub_n];
+        MPI_Recv(buf1, sub_n*sub_n, MPI_INT, A_src, 0, cart_comm, &stat);
+        A = Matrix(buf1, sub_n);
+        int buf2[sub_n*sub_n];
+        MPI_Recv(buf2, sub_n*sub_n, MPI_INT, B_src, 0, cart_comm, &stat);
+        B = Matrix(buf2, sub_n);
         sum = sum + (A * B);
     }
     if (this_rank == 1) {
