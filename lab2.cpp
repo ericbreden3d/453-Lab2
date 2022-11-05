5b66c43311a8a6756492bf6821e1ccbb54cd8a77
Breden <ebrede2@uic.edu>
2022-11-05 16:58:11 -0500

    new

--- lab2.cpp
+++ lab2.cpp
@@ -65,7 +65,7 @@ int main(int argc, char** argv) {
                 int targ_rank;
                 int coord[2] = {i, j};
                 MPI_Cart_rank(cart_comm, coord, &targ_rank);
-                MPI_Isend(parts[ind++].get_1d(), sub_n*sub_n, MPI_INT, targ_rank, 0, cart_comm);
+                MPI_Send(parts[ind++].get_1d(), sub_n*sub_n, MPI_INT, targ_rank, 0, cart_comm);
             }
         }
         
