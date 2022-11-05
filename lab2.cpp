ab34f60bd739c88abe04388149e34f8efefb7044
Breden <ebrede2@uic.edu>
2022-11-05 17:50:58 -0500

    new

--- lab2.cpp
+++ lab2.cpp
@@ -50,12 +50,11 @@ int main(int argc, char** argv) {
         // m.print();
         // (m * m).print();
 
-        double start = MPI_Wtime();
-        serial_result = (m * m).determinant();
-        cout << "Serial result: " << serial_result << endl;
-        cout << "Serial runtime: " << MPI_Wtime() - start << endl;
-        return 0;
-        start = MPI_Wtime();
+        // double start = MPI_Wtime();
+        // serial_result = (m * m).determinant();
+        // cout << "Serial result: " << serial_result << endl;
+        // cout << "Serial runtime: " << MPI_Wtime() - start << endl;
+        // return 0;
 
         Matrix parts[num_procs] = {};
         int ind = 0;
