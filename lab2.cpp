4622777f2ef7ad5be47d29c5011912e692c1a531
Breden <ebrede2@uic.edu>
2022-11-06 13:45:06 -0600

    new

--- lab2.cpp
+++ lab2.cpp
@@ -192,6 +192,7 @@ int main(int argc, char** argv) {
                 }
             }
 
+            cout << "hello";
             cout << "Parallel result: " << multA.determinant() << endl; 
             cout << "Parallel runtime: " << MPI_Wtime() - start << endl;
         }
