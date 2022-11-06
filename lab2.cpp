5b90656371b5001f61b75e978a1cffbb1230232b
Breden <ebrede2@uic.edu>
2022-11-06 13:12:19 -0600

    new

--- lab2.cpp
+++ lab2.cpp
@@ -78,13 +78,13 @@ int main(int argc, char** argv) {
             cout << "Disassembling A" << endl;
             Matrix partsA[num_procs] = {};
             int ind = 0;
-            for (int i = 0; i < n; i+=sub_n) {
-                for (int j = 0; j < n; j+=sub_n) {
-                    cout << sub_n << " " << i << " " << j << endl;
-                    A.print();
-                    partsA[ind++] = A.get_subm(sub_n, i, j);
-                }
-            }
+            // for (int i = 0; i < n; i+=sub_n) {
+            //     for (int j = 0; j < n; j+=sub_n) {
+            //         cout << sub_n << " " << i << " " << j << endl;
+            //         A.print();
+            //         partsA[ind++] = A.get_subm(sub_n, i, j);
+            //     }
+            // }
             cout << "Disassembling B" << endl;
             Matrix partsB[num_procs] = {};
             ind = 0;
