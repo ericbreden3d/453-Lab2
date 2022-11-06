b9c5f262b4c9f056841c27e91891d7ed6d2ab58b
Breden <ebrede2@uic.edu>
2022-11-06 13:31:49 -0600

    new

--- lab2.cpp
+++ lab2.cpp
@@ -94,10 +94,6 @@ int main(int argc, char** argv) {
                 }
             }
 
-            for (int i = 0; i < num_procs; i++) {
-                partsA[i].print();
-            }
-
             cout << "Distributing submatrices" << endl;
             ind = 1;
             for (int i = 0; i < dims[0]; i++) {
@@ -141,10 +137,12 @@ int main(int argc, char** argv) {
         if (this_coord[0] != 0){
             MPI_Recv(buf, sub_n*sub_n, MPI_INT, A_src, 0, cart_comm, &stat);
             A = Matrix(buf, sub_n);
+            cout << "built A" << endl;
         }
         if (this_coord[1] != 0) {
             MPI_Recv(buf, sub_n*sub_n, MPI_INT, B_src, 0, cart_comm, &stat);
             B = Matrix(buf, sub_n);
+            cout << "built B" << endl;
         }
 
         MPI_Barrier(cart_comm);
