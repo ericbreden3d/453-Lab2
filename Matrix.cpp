c104cf5c44b0536288e3df5f0d9ea067480083cc
Breden <ebrede2@uic.edu>
2022-11-06 12:47:53 -0600

    new

--- Matrix.cpp
+++ Matrix.cpp
@@ -16,6 +16,7 @@ Matrix Matrix::get_detrm_subm(int split) {
         int x = split == -1 ? i - 1 : i;  // if already split, shift left
         for (int j = 1; j < this->size; j++) {
             new_m(x, j - 1) = (*this)(i, j);
+            new_m.arr[x + (j - 1) * (size - 1)] = (*this)(i, j);
         }
     }
     return new_m;
