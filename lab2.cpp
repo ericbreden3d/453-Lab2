93b547c8fa2b5f67b90cb1cb806beea05e9e91a7
Breden <ebrede2@uic.edu>
2022-11-06 12:13:44 -0600

    new

--- lab2.cpp
+++ lab2.cpp
@@ -57,98 +57,106 @@ int main(int argc, char** argv) {
             cout << "Serial runtime: " << MPI_Wtime() - start << endl;
             return 0;
         }
+    }
 
-        start = MPI_Wtime();
+    Matrix cur = m;
+    for (int i = 0; i < 2; i++) {
+        if (this_rank == 0) {
+            start = MPI_Wtime();
 
-        Matrix parts[num_procs] = {};
-        int ind = 0;
-        for (int i = 0; i < n; i+=sub_n) {
-            for (int j = 0; j < n; j+=sub_n) {
-                parts[ind++] = m.get_subm(sub_n, i, j);
+            Matrix parts[num_procs] = {};
+            int ind = 0;
+            for (int i = 0; i < n; i+=sub_n) {
+                for (int j = 0; j < n; j+=sub_n) {
+                    parts[ind++] = m.get_subm(sub_n, i, j);
+                }
             }
-        }
 
-        ind = 1;
-        for (int i = 0; i < dims[0]; i++) {
-            for (int j = 0; j < dims[1]; j++) {
-                if (i == 0 && j == 0) continue;
-                int targ_rank;
-                int coord[2] = {i, j};
-                MPI_Cart_rank(cart_comm, coord, &targ_rank);
-                MPI_Send(parts[ind++].get_1d(), sub_n*sub_n, MPI_INT, targ_rank, 0, cart_comm);
+            ind = 1;
+            for (int i = 0; i < dims[0]; i++) {
+                for (int j = 0; j < dims[1]; j++) {
+                    if (i == 0 && j == 0) continue;
+                    int targ_rank;
+                    int coord[2] = {i, j};
+                    MPI_Cart_rank(cart_comm, coord, &targ_rank);
+                    MPI_Send(parts[ind++].get_1d(), sub_n*sub_n, MPI_INT, targ_rank, 0, cart_comm);
+                }
             }
-        }
-        
-        // root doesn't ned to send/recv to itself
-        A = parts[0];
-        B = parts[0];
-    } else {
-        int buf[sub_n * sub_n];
-        MPI_Recv(buf, sub_n * sub_n, MPI_INT, 0, 0, cart_comm, &stat);
-        A = Matrix(buf, sub_n);
-        B = A;
-    }
-
-    // Initial Send Alignment
-    int A_src;
-    int B_src;
-    int A_dest;
-    int B_dest;
-    int buf[sub_n * sub_n];
-    MPI_Cart_shift(cart_comm, 1, -this_coord[0], &A_src, &A_dest);
-    MPI_Cart_shift(cart_comm, 0, -this_coord[1], &B_src, &B_dest);
-    if (this_coord[0] != 0) {
-        MPI_Send(A.get_1d(), sub_n * sub_n, MPI_INT, A_dest, 0, cart_comm);
-    }
-    if (this_coord[1] != 0) {
-        MPI_Send(B.get_1d(), sub_n * sub_n, MPI_INT, B_dest, 0, cart_comm);
-    }
-    if (this_coord[0] != 0){
-        MPI_Recv(buf, sub_n*sub_n, MPI_INT, A_src, 0, cart_comm, &stat);
-        A = Matrix(buf, sub_n);
-    }
-    if (this_coord[1] != 0) {
-        MPI_Recv(buf, sub_n*sub_n, MPI_INT, B_src, 0, cart_comm, &stat);
-        B = Matrix(buf, sub_n);
-    }
-
-    MPI_Barrier(cart_comm);
-    // cout << "Initial alignment complete\n";
-
-    Matrix sum(sub_n);
-    sum = sum + (A * B);
-    for (int i = 1; i < dims[0]; i++) {
-        MPI_Cart_shift(cart_comm, 1, -1, &A_src, &A_dest);
-        MPI_Cart_shift(cart_comm, 0, -1, &B_src, &B_dest);
-        MPI_Send(A.get_1d(), sub_n * sub_n, MPI_INT, A_dest, 0, cart_comm);
-        MPI_Send(B.get_1d(), sub_n * sub_n, MPI_INT, B_dest, 0, cart_comm);
-        MPI_Recv(buf, sub_n*sub_n, MPI_INT, A_src, 0, cart_comm, &stat);
-        A = Matrix(buf, sub_n);
-        MPI_Recv(buf, sub_n*sub_n, MPI_INT, B_src, 0, cart_comm, &stat);
-        B = Matrix(buf, sub_n);
-        sum = sum + (A * B);
-    }
-
-    // collect submatrices at root and assemble matrix
-    if (this_rank != 0) {
-        MPI_Send(sum.get_1d(), sub_n * sub_n, MPI_INT, 0, 0, cart_comm);
-    } else {
-        Matrix parts[num_procs] = {};
-        parts[0] = sum;
-        int ind = 0;
-        for (int i = 1; i < num_procs; i++) {
+            
+            // root doesn't ned to send/recv to itself
+            A = parts[0];
+            B = parts[0];
+        } else {
             int buf[sub_n * sub_n];
-            MPI_Recv(buf, sub_n * sub_n, MPI_INT, i, 0, cart_comm, &stat);
-            int coord[2];
-            MPI_Cart_coords(cart_comm, i, 2, coord);
-            parts[coord[1] + coord[0] * dims[0]] = Matrix(buf, sub_n);
+            MPI_Recv(buf, sub_n * sub_n, MPI_INT, 0, 0, cart_comm, &stat);
+            A = Matrix(buf, sub_n);
+            B = A;
         }
-        Matrix assem(n);
-        ind = 0;
-        for (int i = 0; i < n; i+=sub_n) {
-            for (int j = 0; j < n; j+=sub_n) {
-                assem.add_subm(parts[ind++], sub_n, i, j);
+
+        // Initial Send Alignment
+        int A_src;
+        int B_src;
+        int A_dest;
+        int B_dest;
+        int buf[sub_n * sub_n];
+        MPI_Cart_shift(cart_comm, 1, -this_coord[0], &A_src, &A_dest);
+        MPI_Cart_shift(cart_comm, 0, -this_coord[1], &B_src, &B_dest);
+        if (this_coord[0] != 0) {
+            MPI_Send(A.get_1d(), sub_n * sub_n, MPI_INT, A_dest, 0, cart_comm);
+        }
+        if (this_coord[1] != 0) {
+            MPI_Send(B.get_1d(), sub_n * sub_n, MPI_INT, B_dest, 0, cart_comm);
+        }
+        if (this_coord[0] != 0){
+            MPI_Recv(buf, sub_n*sub_n, MPI_INT, A_src, 0, cart_comm, &stat);
+            A = Matrix(buf, sub_n);
+        }
+        if (this_coord[1] != 0) {
+            MPI_Recv(buf, sub_n*sub_n, MPI_INT, B_src, 0, cart_comm, &stat);
+            B = Matrix(buf, sub_n);
+        }
+
+        MPI_Barrier(cart_comm);
+        // cout << "Initial alignment complete\n";
+
+        Matrix sum(sub_n);
+        sum = sum + (A * B);
+        for (int i = 1; i < dims[0]; i++) {
+            MPI_Cart_shift(cart_comm, 1, -1, &A_src, &A_dest);
+            MPI_Cart_shift(cart_comm, 0, -1, &B_src, &B_dest);
+            MPI_Send(A.get_1d(), sub_n * sub_n, MPI_INT, A_dest, 0, cart_comm);
+            MPI_Send(B.get_1d(), sub_n * sub_n, MPI_INT, B_dest, 0, cart_comm);
+            MPI_Recv(buf, sub_n*sub_n, MPI_INT, A_src, 0, cart_comm, &stat);
+            A = Matrix(buf, sub_n);
+            MPI_Recv(buf, sub_n*sub_n, MPI_INT, B_src, 0, cart_comm, &stat);
+            B = Matrix(buf, sub_n);
+            sum = sum + (A * B);
+        }
+
+        // collect submatrices at root and assemble matrix
+        if (this_rank != 0) {
+            MPI_Send(sum.get_1d(), sub_n * sub_n, MPI_INT, 0, 0, cart_comm);
+        } else {
+            Matrix parts[num_procs] = {};
+            parts[0] = sum;
+            int ind = 0;
+            for (int i = 1; i < num_procs; i++) {
+                int buf[sub_n * sub_n];
+                MPI_Recv(buf, sub_n * sub_n, MPI_INT, i, 0, cart_comm, &stat);
+                int coord[2];
+                MPI_Cart_coords(cart_comm, i, 2, coord);
+                parts[coord[1] + coord[0] * dims[0]] = Matrix(buf, sub_n);
             }
+            Matrix assem(n);
+            ind = 0;
+            for (int i = 0; i < n; i+=sub_n) {
+                for (int j = 0; j < n; j+=sub_n) {
+                    assem.add_subm(parts[ind++], sub_n, i, j);
+                }
+            }
+            
+            if (i != 2)
+                cur = assem;
         }
         // assem.print();
         // cout << "Serial result: " << serial_result << endl;
