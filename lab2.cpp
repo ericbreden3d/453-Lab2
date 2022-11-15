92f2f76ff2aa9ec17caf9314e23689ab8e2f1bcf
Breden <ebrede2@uic.edu>
2022-11-15 16:05:33 -0600

    update

--- lab2.cpp
+++ lab2.cpp
@@ -64,8 +64,8 @@ int main(int argc, char** argv) {
                 result = result * m;
             }
             double serial_runtime = MPI_Wtime() - start;
-            serial_result = result.determinant();
-            cout << "Serial result: " << serial_result << endl;
+            // serial_result = result.determinant();
+            // cout << "Serial result: " << serial_result << endl;
             cout << "Serial runtime: " << serial_runtime << endl;
             cout << endl;
 
@@ -209,7 +209,7 @@ int main(int argc, char** argv) {
             
             if (i == pow - 1) {
                 double parallel_runtime = MPI_Wtime() - start;
-                cout << "Parallel result: " << multA.determinant() << endl; 
+                // cout << "Parallel result: " << multA.determinant() << endl; 
                 cout << "Parallel runtime: " << parallel_runtime << endl;
                 cout << endl;
             }
@@ -217,6 +217,7 @@ int main(int argc, char** argv) {
 
         cout << this_rank << " at barrier 3" << endl;
         MPI_Barrier(cart_comm);
+        // assem.print();
         // cout << "Serial result: " << serial_result << endl;
         // cout << "Parallel result: " << assem.determinant() << endl;
     }
