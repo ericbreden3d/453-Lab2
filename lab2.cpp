5462da6b5d4bbedd57dad3e54463d46268edcf07
Breden <ebrede2@uic.edu>
2022-11-15 15:57:36 -0600

    update

--- lab2.cpp
+++ lab2.cpp
@@ -144,12 +144,12 @@ int main(int argc, char** argv) {
         MPI_Cart_shift(cart_comm, 1, -this_coord[0], &A_src, &A_dest);
         MPI_Cart_shift(cart_comm, 0, -this_coord[1], &B_src, &B_dest);
         if (this_coord[0] != 0) {
+            MPI_Isend(A.get_1d(), sub_n * sub_n, MPI_INT, A_dest, 0, cart_comm, &req);
             cout << this_rank << " sent A to " << A_dest << endl;
-            MPI_Send(A.get_1d(), sub_n * sub_n, MPI_INT, A_dest, 0, cart_comm);
         }
         if (this_coord[1] != 0) {
+            MPI_Isend(B.get_1d(), sub_n * sub_n, MPI_INT, B_dest, 0, cart_comm, &req);
             cout << this_rank << " sent B to " << B_dest << endl;
-            MPI_Send(B.get_1d(), sub_n * sub_n, MPI_INT, B_dest, 0, cart_comm);
         }
         if (this_coord[0] != 0){
             MPI_Recv(buf, sub_n*sub_n, MPI_INT, A_src, 0, cart_comm, &stat);
